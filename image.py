e72d96061bcbebed904d748f6fdd6173962ca2f3
Patryk <patryk17x@gmail.com>
2025-01-12 14:46:45 +0100

    Image, learning openCV

---
+++ image.py
@@ -0,0 +1,27 @@
+import cv2
+
+image_path = 'C:/Users/48605/Desktop/Marta/Urodzinki.png'
+image = cv2.imread(image_path)
+
+cv2.imshow('Image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow('Kamera', frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('x'):
+        print('Zapisywanie obrazu na dysku')
+        cv2.imwrite('Zapisane_zdjecie.jpg', frame)
+        break
+    elif key == ord('q'):
+        print('Opuszczanie programu')
+        break
+
+cap.release()
+cv2.destroyAllWindows()
